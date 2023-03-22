cf34053ef1b1c41ddeae8d515f233edc49baba38
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-03-22 14:25:55 -0400

    added code to server.js and started listening

--- Develop/server.js
+++ Develop/server.js
@@ -4,9 +4,24 @@ const uuid = require('uuid');
 
 const app = express();
 const PORT = process.send.PORT || 3001;
- 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+
+
+// Define Middleware
+const errorHandler = require('./middleware/errorHandler');
+
+// Define Routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
